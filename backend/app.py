7e1b94ef794034a7a0ea7fe73ab42166582fd0a9
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2024-04-25 23:24:48 -0300

    feat: sign-up users route

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, request
+from supabase_package.client import supabase
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return 'Hello, World!'
+@app.route('/users/create', methods=['POST'])
+def users():
+  if request.method == 'POST':
+    user_email = request.form.get('email')
+    user_password = request.form.get('password')
 
-@app.route('/<name>')
-def print_name(name):
-  return 'Hi, {}'.format(name)
+    supabase.auth.sign_up(credentials={ 'email': user_email, 'password': user_password })
+
+    return {
+      'message': 'User created successfully.'
+    }, 201
+  else:
+    return {
+      'message': 'Method not allowed, try POST.'
+    }, 404
 
 if __name__ == '__main__':
   app.run(debug=True)
