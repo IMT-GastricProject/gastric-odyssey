d714dd6d3108960fba89d083aba28c9161af9dcf
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2024-04-25 23:30:09 -0300

    fix: fix screen-size variables

--- frontend/settings.py
+++ frontend/settings.py
@@ -1,6 +1,9 @@
 from screeninfo import get_monitors
 
 #ajusta o tamanho da tela para o tamanho do monitor principal da pessoa
+WIDTH = 0
+HEIGHT = 0
+
 for monitor in get_monitors():
     if monitor.is_primary == True:
         WIDTH = monitor.width
