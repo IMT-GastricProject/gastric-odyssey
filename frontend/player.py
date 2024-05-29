70d6865e2c1af8b81817d4c8b947bbd63af11cf2
Matheus Castilho <cassmatheuss@gmail.com>
2024-05-29 00:24:59 -0300

    fix: player speed

--- frontend/player.py
+++ frontend/player.py
@@ -15,7 +15,7 @@ class Player(pygame.sprite.Sprite):
         #define a direção como um vetor de duas dimensões (x,y)
         self.direction = pygame.math.Vector2()
         #define a velocidade da qual o sprite se movimenta, pois é multiplicado pelo valor do vetor direction
-        self.speed = 55
+        self.speed = 15
         #define o obstacle_sprites que armazena os obstáculos que o player colide, que é passado como argumento de Player
         self.obstacle_sprites = obstacle_sprites
         self.dont_touch = dont_touch
